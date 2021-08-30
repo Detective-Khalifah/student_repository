9eec5471a65d01529df67cf1cc536398274bd4d2
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-08-31 00:44:30 +0100

    Add theming to appBar Set colorSwatch to lightGreen

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        appBarTheme: AppBarTheme(centerTitle: true, elevation: 2),
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
       home: WelcomePage(title: 'Flutter Demo Home Page'),
     );
