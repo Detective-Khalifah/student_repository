1b93ef2baea0514b35a75c1c513633b16e753f28
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-09-01 02:42:12 +0100

    Add eight more rows to the {@link ListView} at {@link HomePage} Create an instance of {@link Firestore} and use it to access the Firestore database by Firebase; traverse the documents, seeking a document with a match to the currently logged in user's e-mail address; print a message to console stating the result. Add a log out {@link IconButton} on the {@link AppBar} at {@link HomePage}

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:student_repository/services/authentication.dart';
+
+final _firestore = FirebaseFirestore.instance;
 
 class HomePage extends StatefulWidget {
   static const String id = 'home';
+
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -13,13 +20,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<Authentication>().logOut();
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.directions_run),
+          )
+        ],
+      ),
       backgroundColor: Colors.yellowAccent,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: ListView(
           scrollDirection: Axis.vertical,
           children: [
+            DataStream(),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: CircleAvatar(
@@ -40,11 +58,86 @@ class _HomePageState extends State<HomePage> {
                     DataCell(Text('A.')),
                   ],
                 ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('First Name')),
+                    DataCell(Text('A.')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('First Name')),
+                    DataCell(Text('A.')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('First Name')),
+                    DataCell(Text('A.')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('First Name')),
+                    DataCell(Text('A.')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('First Name')),
+                    DataCell(Text('A.')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('First Name')),
+                    DataCell(Text('A.')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('First Name')),
+                    DataCell(Text('A.')),
+                  ],
+                ),
+                DataRow(
+                  cells: [
+                    DataCell(Text('First Name')),
+                    DataCell(Text('A.')),
+                  ],
+                ),
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class DataStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('students').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.grey,
+            ),
+          );
+        }
+
+        final students = snapshot.data!.docs;
+        for (var student in students) {
+          if (student.get('e-mail') == context.read<User>().email) {
+            print('User ${context.read<User>().email} located!');
+          } else
+            print('Couldn\'t find user');
+        }
+        return Text('Eureka!');
+      },
     );
   }
 }
