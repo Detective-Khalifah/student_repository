e04ca786a2313942a8e36a9773c634f1bf4c11a7
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-09-01 16:22:05 +0100

    Add course editing & viewing pages Add profile editing page

---
+++ lib/pages/edit_courses_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditCoursesPage extends StatefulWidget {
+  const EditCoursesPage({Key? key}) : super(key: key);
+
+  @override
+  _EditCoursesPageState createState() => _EditCoursesPageState();
+}
+
+class _EditCoursesPageState extends State<EditCoursesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/view_courses_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewCoursesPage extends StatefulWidget {
+  const ViewCoursesPage({Key? key}) : super(key: key);
+
+  @override
+  _ViewCoursesPageState createState() => _ViewCoursesPageState();
+}
+
+class _ViewCoursesPageState extends State<ViewCoursesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
