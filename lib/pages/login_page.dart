de6485b826f85d44536cfcfbb4cb1bf85e927ea2
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-09-03 01:17:13 +0100

    Fix: {@link LoginPage} Widgets not displaying due to being wrapped in {@link Flexible}, ironically to fix the issue of "pixel overflow"

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -38,83 +38,81 @@ class _LoginPageState extends State<LoginPage> {
           key: _logFormKey,
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Flexible(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  TextFormField(
-                    controller: _emailControl,
-                    textAlign: TextAlign.center,
-                    keyboardType: TextInputType.emailAddress,
-                    onChanged: (value) {},
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'E-mail must be entered!';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'E-mail',
-                    ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  controller: _emailControl,
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {},
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'E-mail must be entered!';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
                   ),
-                  SizedBox(
-                    height: 8.0,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextFormField(
+                  controller: _pwdControl,
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {},
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password must be entered!';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Password',
                   ),
-                  TextFormField(
-                    controller: _pwdControl,
-                    obscureText: true,
-                    textAlign: TextAlign.center,
-                    onChanged: (value) {},
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Password must be entered!';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 24.0,
-                  ),
-                  Hero(
-                    tag: 'log',
-                    child: RoundedButton(
-                      colour: Colors.greenAccent,
-                      label: 'Log In',
-                      onPressed: () async {
-                        setState(() {
-                          loggingIn = true;
-                        });
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                Hero(
+                  tag: 'log',
+                  child: RoundedButton(
+                    colour: Colors.greenAccent,
+                    label: 'Log In',
+                    onPressed: () async {
+                      setState(() {
+                        loggingIn = true;
+                      });
 
-                        // Validate returns true if the form is valid, or false otherwise.
-                        if (_logFormKey.currentState!.validate()) {
-                          // If the form is valid, display a snackbar. In the real world,
-                          // you'd often call a server or save the information in a database.
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Processing Data')),
-                          );
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_logFormKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
 
-                          final login = await context
-                              .read<Authentication>()
-                              .logIn(
-                                  email: _emailControl.text,
-                                  pwd: _pwdControl.text);
-                          if (login == "Logged in!")
-                            Navigator.pushNamed(context, HomePage.id);
-                        } else {
-                          // _logFormKey.currentState!.reset();
-                        }
-                        setState(() {
-                          loggingIn = false;
-                        });
-                      },
-                    ),
+                        final login = await context
+                            .read<Authentication>()
+                            .logIn(
+                                email: _emailControl.text,
+                                pwd: _pwdControl.text);
+                        if (login == "Logged in!")
+                          Navigator.pushNamed(context, HomePage.id);
+                      } else {
+                        // _logFormKey.currentState!.reset();
+                      }
+                      setState(() {
+                        loggingIn = false;
+                      });
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
